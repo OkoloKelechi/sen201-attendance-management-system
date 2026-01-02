4fe0e000cf0b17772db9e6e80ae97a9dec55008b
OkoloKelechi <okolokelechip@gmail.com>
2026-01-02 01:35:57 +0100

    Add files via upload

---
+++ attendance_management_system.py
@@ -0,0 +1,6 @@
+student_name = input("Enter student name: ")
+attendance_status = input("Enter attendance status (Present/Absent): ")
+
+print("\n--- Attendance Record ---")
+print("Student Name:", student_name)
+print("Attendance Status:", attendance_status)
